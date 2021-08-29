ff95663d898622abcff584a222bd13e7b1953fc1
Andrew Welc <endriuwelc@gmail.com>
2021-08-29 19:53:15 +0200

    configure eslint, delete default css, add articles and custom css

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from "components/Layout";
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <Layout>
+      <Head>
+        <title>About - o mnie</title>
+      </Head>
+      <p>O mnie treść</p>
+    </Layout>
+  )
+}
